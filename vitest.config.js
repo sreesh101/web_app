56525d7e3d850b3436dff88f951b3cec2f1d7ef7
sreesh_p <poudyalsreesh@gmail.com>
2023-12-11 01:06:39 +0700

    Completed

--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,7 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import {defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    }
-  })
-)
+export default defineConfig({
+  base: '/web_app/',
+  plugins: [vue()]
+})
